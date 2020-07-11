a1d8eadde0b470f8ad6fecb8afbf118e82f6118f
Marco Pettorali <marcopettorali1@gmail.com>
2020-07-11 19:56:25 +0200

    Added observer to Actuator class

--- src/main/java/Actuator.java
+++ src/main/java/Actuator.java
@@ -1,12 +1,15 @@
 import java.net.InetAddress;
+import java.util.List;
 
 public class Actuator extends Device {
     ResourceConnection resource;
+    private DoubleObserver observer;
 
     public Actuator(InetAddress address, int room, String type, String metric, Integer deviceId) {
         super(address, room, type, metric, deviceId);
         String observedResourceAddress = "coap://[" + address.getHostAddress() + "]/" + metric + "_actuator";
         this.resource = new ResourceConnection(observedResourceAddress);
+        this.observer = new DoubleObserver(observedResourceAddress, 256);
     }
 
     public String sendGetRequest(String[] params) {
@@ -19,5 +22,9 @@ public class Actuator extends Device {
 
     public void sendPutRequest(String jsonString) {
         resource.sendPutRequest(jsonString);
+    }
+
+    public List<Double> getDataSince(long date) {
+        return observer.getDataSince(date);
     }
 }
