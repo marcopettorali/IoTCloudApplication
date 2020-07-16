94513e15a147f242bfe7f61157701ddffa8e5007
Marco Pettorali <marcopettorali1@gmail.com>
2020-07-16 15:50:20 +0200

    Added documentation, added setPeriod() in Sensor

--- src/main/java/com/aquarium/lln_interface/Sensor.java
+++ src/main/java/com/aquarium/lln_interface/Sensor.java
@@ -5,11 +5,17 @@ import java.util.List;
 
 public class Sensor extends Device {
     protected DoubleObserver observer;
+    ResourceConnection resource;
 
     public Sensor(InetAddress address, int room, String type, String metric, Integer deviceId) {
         super(address, room, type, metric, deviceId);
         String observedResourceAddress = "coap://[" + address.getHostAddress() + "]/" + metric;
         this.observer = new DoubleObserver(observedResourceAddress, 256, observedResourceAddress);
+        this.resource = new ResourceConnection(observedResourceAddress);
+    }
+
+    public void setPeriod(double value) {
+        resource.sendPutRequest("P=" + value);
     }
 
     public List<Double> getDataSince(long date) {
